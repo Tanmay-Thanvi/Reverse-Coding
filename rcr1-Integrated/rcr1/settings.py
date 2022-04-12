98cf46e3be96ac3bdbeaff7e3e7f431086c4f16d
Tanmay Thanvi <78643125+Tanmay-Thanvi@users.noreply.github.com>
2022-04-12 23:55:34 +0530

    db changed

--- rcr1-Integrated/rcr1/settings.py
+++ rcr1-Integrated/rcr1/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'rcr1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'rcr1_22.sqlite3',
     }
 }
 
